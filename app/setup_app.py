7c4b867a66198e2a3403e8efcb59933c97f4c040
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2020-04-25 22:16:20 +0300

    Изменена структура проекта (app/blueprints->app/views, app/verify_email.py->app/auth_utils.py), добавлен обработчик и шаблон для профиля пользователя (пока только наработки), добавлен ресурс авторизации (генерирует токены для доступа к API). Добавлен загрузчик пользователя, загрузчик пользователя через запрос. Обновлён FlaskIO. Некоторые куски из index.jinja2 вынесены в index_addition.jinja2 и index_photo_addition.jinja2. И другие изменения по мелочи.

--- app/setup_app.py
+++ app/setup_app.py
@@ -1,13 +1,14 @@
 """Модуль, отвечающий за создание различных частей приложения, которые будут
 прикреплены к нему на этапе инициализации"""
 
-import functools
-
-from flask_login import current_user
+import jwt
 from flask_login.login_manager import LoginManager
 from flask_mail import Mail
 from flask_socketio import SocketIO
-from flask_socketio import disconnect
+
+from app.auth_utils import decode_auth_token
+from app.data import db_session
+from app.data.users import Users
 
 login_manager = LoginManager()
 mail = Mail()
@@ -16,33 +17,30 @@ socketio = SocketIO()
 
 @login_manager.user_loader
 def load_user(user_id):
-    # TODO Заглушка. Переделать
-    return
+    session = db_session.create_session()
+    return session.query(Users).filter(Users.alternative_id == user_id).first()
 
 
-def authenticated_only(f):
-    @functools.wraps(f)
-    def wrapped(*args, **kwargs):
-        if not current_user.is_authenticated:
-            disconnect()
-        else:
-            return f(*args, **kwargs)
-
-    return wrapped
-
-# @login_manager.request_loader
-# def load_user_from_request(request):
-#     print('request_loader called')
-#     auth_headers = request.headers.get('Authorization', '').split()
-#     if len(auth_headers) == 2 and auth_headers[0] == 'Bearer':
-#         token = auth_headers[1]
-#     elif request.json and 'access_token' in request.json:
-#         token = request.json['access_token']
-#     else:
-#         return
-#     try:
-#         data = jwt.decode(token, current_app.config['SECRET_KEY'])
-#     except jwt.ExpiredSignatureError:
-#         return
-#     except (jwt.InvalidTokenError, Exception) as e:
-#         return
+@login_manager.request_loader
+def load_user_from_request(request):
+    """Загрузка пользователя через запрос"""
+    auth_headers = request.headers.get('Authorization', '').split()
+    if len(auth_headers) == 2 and auth_headers[0] == 'Bearer':
+        token = auth_headers[1]
+    elif request.json and 'access_token' in request.json:
+        token = request.json['access_token']
+    else:
+        # abort(
+        #     401,
+        #     'The access token not specified. Specify the token in the '
+        #     '"Authorization" header with the Bearer type, or pass it in the '
+        #     '"access_key" request parameter'
+        # )
+        return
+    try:
+        data = decode_auth_token(token)
+    except jwt.exceptions.InvalidTokenError:
+        return
+    user_id = data['aud']
+    session = db_session.create_session()
+    return session.query(Users).filter(Users.alternative_id == user_id).first()
