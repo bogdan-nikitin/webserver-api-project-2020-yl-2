462abcc1e62068647d35023949bf40e975f5679e
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2020-04-30 22:44:37 +0300

    Улучшил некоторые моменты, добавил шаблон для документации

--- app/views/docs.py
+++ app/views/docs.py
@@ -1,10 +1,15 @@
 from flask import Blueprint, render_template
 from modules.constants import ROOT_DIR
-
-blueprint = Blueprint('docs', __name__,
-                      template_folder=ROOT_DIR + '/app/docs/cached')
+import os
 
 
-@blueprint.route('/docs/v1/api_docs')
+blueprint = Blueprint(
+    'docs', __name__,
+    template_folder=os.path.join(ROOT_DIR, 'app/docs/cached'),
+    url_prefix='/docs/v1/'
+)
+
+
+@blueprint.route('api_docs')
 def api_docs():
     return render_template('api_docs.jinja2')
