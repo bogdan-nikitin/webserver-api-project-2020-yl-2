a1fd5b8f5a18a70d73987683fcba5117cf8deea9
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2020-05-05 00:08:14 +0300

    Исправил баг, при котором тесты вылетали после 1-го

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -1,6 +1,5 @@
 import os
 
-
 cur_file = os.path.basename(__file__)
 
 print('starting tests...')
@@ -8,5 +7,8 @@ print('starting tests...')
 for test_file in os.listdir():
     if test_file != cur_file:
         print('running', test_file)
-        __import__(test_file)
+        try:
+            __import__(test_file)
+        except ModuleNotFoundError:
+            pass
 print('tests finished')
