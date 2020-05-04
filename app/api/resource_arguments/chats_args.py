bc68eb21a17e4df56cf6fb4fa3b39a8b2d0a60eb
EvolutionSup <avanothermail@gmail.com>
2020-05-04 23:06:53 +0300

    Добавил тесты, изменил модель Chats и все, что с ней связано

--- app/api/resource_arguments/chats_args.py
+++ app/api/resource_arguments/chats_args.py
@@ -2,8 +2,8 @@ from flask_restful import reqparse
 
 post_parser = reqparse.RequestParser()
 post_parser.add_argument('title')
-post_parser.add_argument('first_author_id', required=True, type=int)
-post_parser.add_argument('second_author_id', required=True, type=int)
+post_parser.add_argument('first_author_id', required=True, type=str)
+post_parser.add_argument('second_author_id', required=True, type=str)
 # список объектов класса Users(реализация через ChatParticipants)
 # post_parser.add_argument('chat_participants', required=True, type=list)
 
--- app/data/chats.py
+++ app/data/chats.py
@@ -17,11 +17,13 @@ class Chats(SqlAlchemyBase, SerializerMixin):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    first_author_id = sqlalchemy.Column(sqlalchemy.Integer,
-                                        sqlalchemy.ForeignKey('users.id'),
+    first_author_id = sqlalchemy.Column(sqlalchemy.String,
+                                        sqlalchemy.ForeignKey(
+                                            'users.alternative_id'),
                                         nullable=False)
-    second_author_id = sqlalchemy.Column(sqlalchemy.Integer,
-                                         sqlalchemy.ForeignKey('users.id'),
+    second_author_id = sqlalchemy.Column(sqlalchemy.String,
+                                         sqlalchemy.ForeignKey(
+                                             'users.alternative_id'),
                                          nullable=False)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True, default=None)
     chat_participants = orm.relation('ChatParticipants', backref='chats')
